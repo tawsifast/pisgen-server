a0a7cffcb5a12b3f18c5ef9febd617af3b72b249
tawsifast <0070tawsif@gmail.com>
2026-04-28 16:13:19 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+     <Banner/>
+    
+    </div>
+  );
+}
